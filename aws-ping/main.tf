ec6afb977372a32c801cd45f060ee185a08f4d13
Akos Fulop <akos.fulop.github@gmail.com>
2023-09-24 10:16:50 +0200

    Change ami ID for testing

--- aws-ping/main.tf
+++ aws-ping/main.tf
@@ -22,7 +22,7 @@ provider "aws" {
 }
 
 resource "aws_instance" "app_server" {
-  ami           = "ami-01342111f883d5e4e"
+  ami           = "ami-0f845a2bba44d24b2"
   instance_type = "t2.micro"
 
   tags = {
