ed331c5db858fc5bb9a6492ae92a39ed68c2b118
Akos Fulop <akos.fulop.github@gmail.com>
2023-09-24 09:57:03 +0200

    Use terraform fmt as per recommendation

--- aws-ping/main.tf
+++ aws-ping/main.tf
@@ -1,31 +1,31 @@
 variable "region" {
-	default = "eu-central-1"
+  default = "eu-central-1"
 }
 
 variable "profile" {
-	default = "terraform"
+  default = "terraform"
 }
 
 terraform {
-	required_providers {
-		aws = {
-			source  = "hashicorp/aws"
-			version = "~> 4.16"
-		}
-	}
-	required_version = ">= 1.2.0"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.16"
+    }
+  }
+  required_version = ">= 1.2.0"
 }
 
-provider "aws"{
-	region=var.region
-	profile=var.profile
+provider "aws" {
+  region  = var.region
+  profile = var.profile
 }
 
 resource "aws_instance" "app_server" {
-	ami           = "ami-01342111f883d5e4e"
-	instance_type = "t2.micro"
+  ami           = "ami-01342111f883d5e4e"
+  instance_type = "t2.micro"
 
-	tags = {
-		Name = "ExampleAppServerInstance"
-	}
+  tags = {
+    Name = "ExampleAppServerInstance"
+  }
 }
