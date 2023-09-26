66b7ad3ef52438404787c6ff3480d894150d2b3e
Akos Fulop <akos.fulop.github@gmail.com>
2023-09-26 09:58:09 +0200

    Add personal ssh_key

--- main.tf
+++ main.tf
@@ -29,6 +29,11 @@ resource "aws_iam_account_alias" "alias" {
   account_alias = "akos-fulop"
 }
 
+resource "aws_key_pair" "akos_personal" {
+  key_name   = "akos-notebook"
+  public_key = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAILkLaudn7GAkiW8Z7/GVas6EjCQ2lf+/oFPhRCK5P5aD akos.fulop.github@gmail.com"
+}
+
 module "ec2_practice_env" {
   source = "./ec2practice"
   count = local.deploy_ec2_playground == true ? 1 : 0
