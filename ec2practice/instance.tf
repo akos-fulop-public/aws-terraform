e072951aa7de1090b2db11d8ba28e0bbca41a522
Akos Fulop <akos.fulop.github@gmail.com>
2023-09-27 13:35:22 +0200

    Add security group for SSH and attack key and security group to EC2 instance

--- ec2practice/instance.tf
+++ ec2practice/instance.tf
@@ -5,4 +5,32 @@ resource "aws_instance" "app_server" {
   tags = {
     Name = "ExampleAppServerInstance"
   }
+
+  key_name               = "akos-notebook"
+  vpc_security_group_ids = [aws_security_group.ssh_inbound.id]
+}
+
+data "aws_vpc" "default" {
+  default = true
+}
+
+resource "aws_security_group" "ssh_inbound" {
+  name = "allow_ssh"
+  description = "Allow SSH"
+  vpc_id = data.aws_vpc.default.id
+
+  ingress {
+    description = "SSH"
+    from_port = 22
+    to_port = 22
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port = 0
+    to_port = 0
+    protocol = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 }
