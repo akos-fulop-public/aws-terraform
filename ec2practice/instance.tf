c5676deb8e52e90bd1e8bbba246e44837b24427e
Akos Fulop <akos.fulop.github@gmail.com>
2023-10-01 09:56:09 +0300

    Associate aws instance with target group

--- ec2practice/instance.tf
+++ ec2practice/instance.tf
@@ -73,3 +73,9 @@ resource "aws_lb_target_group" "test_target_group" {
   protocol = "HTTP"
   vpc_id = data.aws_vpc.default.id
 }
+
+resource "aws_lb_target_group_attachment" "test" {
+  target_group_arn = aws_lb_target_group.test_target_group.arn
+  target_id        = aws_instance.app_server.id
+  port             = 80
+}
