055d7dd2306b898e1353faa5a387f8979f766d33
Akos Fulop <akos.fulop.github@gmail.com>
2023-10-03 09:36:59 +0300

    Add listener for the load balancer

--- ec2practice/instance.tf
+++ ec2practice/instance.tf
@@ -90,3 +90,14 @@ resource "aws_lb" "test" {
   security_groups    = [aws_security_group.http_inbound.id,aws_security_group.internet.id]
   subnets            = data.aws_subnets.default_subnets.ids
 }
+
+resource "aws_lb_listener" "load_balancer_listener" {
+  load_balancer_arn = aws_lb.test.arn
+  port              = "80"
+  protocol          = "HTTP"
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.test_target_group.arn
+  }
+}
