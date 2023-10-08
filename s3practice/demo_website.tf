9b7b2d7b99b896554e7c98b7d0a6716839f6f19c
Akos Fulop <akos.fulop.github@gmail.com>
2023-10-08 10:33:38 +0300

    Add index.html file

--- s3practice/demo_website.tf
+++ s3practice/demo_website.tf
@@ -1,3 +1,9 @@
-resource "aws_s3_bucket" "website-bucket" {
+resource "aws_s3_bucket" "website_bucket" {
   bucket = "akos-website-demo"
+}
+
+resource "aws_s3_object" "index_html" {
+  bucket = aws_s3_bucket.website_bucket.id
+  key = "index.html"
+  source = "${path.module}/index.html"
 }
