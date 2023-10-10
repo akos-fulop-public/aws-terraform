039fd5dc22154785a95da45437b490b7a0de4ddf
Akos Fulop <akos.fulop.github@gmail.com>
2023-10-10 12:18:37 +0300

    Change index file content type to html

--- s3practice/demo_website.tf
+++ s3practice/demo_website.tf
@@ -6,6 +6,7 @@ resource "aws_s3_object" "index_html" {
   bucket = aws_s3_bucket.demo_bucket.id
   key = "index.html"
   source = "${path.module}/index.html"
+  content_type = "text/html"
 }
 
 resource "aws_s3_bucket_public_access_block" "demo_bucket_public_access" {
